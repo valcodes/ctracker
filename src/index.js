a5ab9f29dcee3813883847f93fc8ff9dcffaaaff
Valentina Abramova <vabramova1@gmail.com>
2018-02-23 16:06:37 -0600

    deploy try 2

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import registerServiceWorker from "./registerServiceWorker";
+// import registerServiceWorker from "./registerServiceWorker";
 
 import { BrowserRouter } from "react-router-dom";
 
@@ -13,4 +13,4 @@ ReactDOM.render(
 
   document.getElementById("root")
 );
-registerServiceWorker();
+// registerServiceWorker();
